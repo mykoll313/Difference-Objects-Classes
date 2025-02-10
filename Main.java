7c6ed5c0d820d1c1162235748dc746f328896212
mykoll313 <michaelawilliams313@gmail.com>
2025-02-10 01:18:35 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -20,12 +20,4 @@ public class Main {
 	}
 
 }
-//Class = Blueprint
-class Car {
-    String brand;
-    int speed;
 
-    void drive() {
-        System.out.println(brand + " is driving at " + speed + " mph!");
-    }
-}
